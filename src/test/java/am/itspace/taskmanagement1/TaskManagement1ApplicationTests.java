595496eef47029743f8999f5ca3e8f70bdcc72d3
Alina-Vardanovna <alinavardanovna@gmail.com>
2022-09-21 00:53:02 +0400

    Initial commit

---
+++ src/test/java/am/itspace/taskmanagement1/TaskManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.taskmanagement1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
